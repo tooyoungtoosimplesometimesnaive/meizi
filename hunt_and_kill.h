860b3d16ec21c26815dbf34ff6da44f1ad21b9fc
Xing <xingmarc@yeah.net>
2018-04-02 22:52:59 -0700

    Implement hunt and kill.

--- hunt_and_kill.h
+++ hunt_and_kill.h
@@ -1,7 +1,6 @@
 #include "grid.h"
 #include "cell.h"
 #include <vector>
-#include <iostream>
 
 int uniform_dist(int start, int end)// all inclusive
 {
@@ -19,11 +18,10 @@ public:
 		while (current != nullptr)
 		{
 			std::vector<Cell*> unvisited_neighbors({});
-			for (auto it = current->neighbors().begin(); it != current->neighbors().end(); it++)
+			auto n = current->neighbors();
+
+			for (auto it = n.begin(); it != n.end(); ++it)
 			{
-				std::cout << "here in the initial loop" << std::endl;
-				std::cout << (*it)->links.empty() << std::endl;
-				std::cout << unvisited_neighbors.size() << std::endl;
 				if ((*it)->links.empty())
 				{
 					unvisited_neighbors.push_back(*it);
@@ -32,9 +30,6 @@ public:
 
 			if (!unvisited_neighbors.empty())
 			{
-				std::cout << "here" << std::endl;
-				//select a random neighbor
-				// current = neighbor
 				int r = uniform_dist(0, unvisited_neighbors.size() - 1);
 				auto neighbor = unvisited_neighbors[r];
 				current->link(neighbor);
@@ -45,12 +40,12 @@ public:
 				{
 					for (auto itc = itr->begin(); itc != itr->end(); itc++)
 					{
-				std::cout << "here in the loop" << std::endl;
 						std::vector<Cell*> visited_neighbors({});
-						for (auto n = itc->neighbors().begin(); n != itc->neighbors().end(); n++)
+						auto neighbors = itc->neighbors();
+						for (auto itn = neighbors.begin(); itn != neighbors.end(); itn++)
 						{
-							if (!(*n)->links.empty()) {
-								visited_neighbors.push_back(*n);
+							if (!(*itn)->links.empty()) {
+								visited_neighbors.push_back(*itn);
 							}
 						}
 
