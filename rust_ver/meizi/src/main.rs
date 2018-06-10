df1a2023c5646aa70f1df96c06d2b66c19e1895e
Xing <xingmarc@yeah.net>
2018-06-09 23:58:48 -0700

    First rust try.

--- rust_ver/meizi/src/main.rs
+++ rust_ver/meizi/src/main.rs
@@ -1,3 +1,27 @@
+#[derive(Debug)]
+struct Cell {
+    row: i32,
+    column: i32,
+    value: i32
+}
+
+struct Grid {
+    grid: Vec<Vec<Cell>>
+}
+
 fn main() {
+    let mut v = 1;
+    let mut _grid = vec![];
+    for r in 1..10 {
+        let mut vector_row = vec![];
+        for c in 1..10 {
+            let cell = Cell{row: r, column: c, value: v};
+            v = v + 1;
+            vector_row.push(cell);
+        }
+        _grid.push(vector_row);
+    }
+    let g = Grid { grid: _grid };
+
     println!("Hello, world!");
 }
